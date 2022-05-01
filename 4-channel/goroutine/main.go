c17904015f5e91dada2bcaa71785ddf02fcc8839
Shunpoco <tkngsnsk313320@gmail.com>
2022-05-01 14:10:08 +0900

    Use sample in the text

--- 4-channel/goroutine/main.go
+++ 4-channel/goroutine/main.go
@@ -5,31 +5,14 @@ import (
 	"time"
 )
 
-func a() {
-	fmt.Println("A!")
+func sub() {
+	fmt.Println("sub() is running")
+	time.Sleep(time.Second)
+	fmt.Println("Sub() is finished")
 }
 
 func main() {
-	// Standard function call
-	a()
-
-	// Define an anonymous function and call it
-	func() {
-		fmt.Println("This is an anonymous function!")
-	}()
-
-	// Create an another goroutine and call a
-	// probably it does not print anything
-	// because main() is finished before this goroutine is finished.
-	go a()
-
-	// Define an anonymous function, then call it on an another goroutine
-	go func() {
-		// if does not print anything
-		fmt.Println("This is an anonymous function 2!")
-	}()
-
-	// It's a very naive solution: just wait a second.
-	// Goroutines may be finished.
-	time.Sleep(100000)
+	fmt.Println("Start sub()")
+	go sub()
+	time.Sleep(2 * time.Second)
 }
