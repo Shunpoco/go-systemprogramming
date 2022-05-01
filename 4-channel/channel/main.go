83bf6e9b48140b7afca1977bec9455c705d49ef6
Shunpoco <tkngsnsk313320@gmail.com>
2022-05-01 14:28:08 +0900

    get data from closed channel

--- 4-channel/channel/main.go
+++ 4-channel/channel/main.go
@@ -10,16 +10,22 @@ func main() {
 	// the channel in order to notice the end of goroutine.
 	// we can use any types
 	// Use struct, because it is 0byte.
-	done := make(chan struct{})
+	done := make(chan int)
 	go func() {
 		fmt.Println("sub() is start!")
 		time.Sleep(time.Second)
 		fmt.Println("sub() is finished")
-		done <- struct{}{}
+		done <- 1
 	}()
 
 	// Wait the finish
 	<-done
 
 	fmt.Println("all tasks are finished")
+
+	close(done)
+
+	// After a channel is closed, we still can read data from the channel.
+	// We get default value. In this case, int(0).
+	fmt.Println(<-done)
 }
