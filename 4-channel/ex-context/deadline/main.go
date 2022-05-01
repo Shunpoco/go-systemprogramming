e7727e19fb02eceaa2cb178746b7821fda6f62db
Shunpoco <tkngsnsk313320@gmail.com>
2022-05-01 21:42:46 +0900

    Use context.WithDeadline

--- 4-channel/ex-context/deadline/main.go
+++ 4-channel/ex-context/deadline/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -8,7 +9,7 @@ import (
 
 var wg sync.WaitGroup
 
-func generator(done chan struct{}, num int) <-chan int {
+func generator(ctx context.Context, num int) <-chan int {
 	out := make(chan int)
 	go func() {
 		defer wg.Done()
@@ -16,7 +17,7 @@ func generator(done chan struct{}, num int) <-chan int {
 	LOOP:
 		for {
 			select {
-			case <-done:
+			case <-ctx.Done():
 				break LOOP
 				// It pretends to take long time
 				// case out <- num:
@@ -30,23 +31,27 @@ func generator(done chan struct{}, num int) <-chan int {
 }
 
 func main() {
-	done := make(chan struct{})
-	gen := generator(done, 1)
-	deadlineChan := time.After(time.Second)
+	// A context created by context.WithDeadline is automatically closed if the time goes to the second value.
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second))
+	gen := generator(ctx, 1)
 
 	wg.Add(1)
 
 LOOP:
 	for i := 0; i < 5; i++ {
 		select {
-		case result := <-gen:
-			fmt.Println(result)
-		case <-deadlineChan:
-			fmt.Println("timeout")
-			break LOOP
+		case result, ok := <-gen:
+			if ok {
+				fmt.Println(result)
+			} else {
+				fmt.Println("timeout")
+				break LOOP
+			}
 		}
 	}
-	close(done)
+
+	// We can also use cancel as an explicitly close
+	cancel()
 
 	wg.Wait()
 }
