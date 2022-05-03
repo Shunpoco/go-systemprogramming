9d71000b486ca136f1e74e94976874f0d4fd7df3
Shunpoco <tkngsnsk313320@gmail.com>
2022-05-04 00:53:48 +0900

    User package defined key

--- 4-channel/ex-context/value-tips/hoge/hoge.go
+++ 4-channel/ex-context/value-tips/hoge/hoge.go
@@ -5,11 +5,17 @@ import (
 	"fmt"
 )
 
+type ctxKey int
+
+const (
+	a ctxKey = iota
+)
+
 func SetValue(ctx context.Context) context.Context {
-	return context.WithValue(ctx, "a", "b")
+	return context.WithValue(ctx, a, "b")
 }
 
 func GetValueFromHoge(ctx context.Context) {
-	val, ok := ctx.Value("a").(string)
+	val, ok := ctx.Value(a).(string)
 	fmt.Println(val, ok)
 }
