0abbe9bdc1c4afb16a1f1f4a7f8e5e8fdd9b0652
Shunpoco <tkngsnsk313320@gmail.com>
2023-01-02 12:57:42 +0900

    HTTP client

--- 6-tcp-http/client/main.go
+++ 6-tcp-http/client/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"net/http"
+	"net/http/httputil"
 )
 
 func main() {
@@ -10,13 +13,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprintf(conn, "hoge")
-	conn.Close()
-
-	conn, err = net.Dial("tcp", "localhost:9999")
+	request, err := http.NewRequest(
+		"GET", "/", nil,
+	)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprintf(conn, "fuga")
-	conn.Close()
+
+	request.Write(conn)
+	response, err := http.ReadResponse(
+		bufio.NewReader(conn), request,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	dump, err := httputil.DumpResponse(response, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
 }
