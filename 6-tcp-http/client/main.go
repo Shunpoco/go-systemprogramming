860bc6578c02faa934a179c0af080dd7c3d99e5c
Shunpoco <tkngsnsk313320@gmail.com>
2023-01-02 14:12:04 +0900

    HTTP client KeepAlive

--- 6-tcp-http/client/main.go
+++ 6-tcp-http/client/main.go
@@ -6,31 +6,60 @@ import (
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:9999")
-	if err != nil {
-		panic(err)
-	}
-	request, err := http.NewRequest(
-		"GET", "/", nil,
-	)
-	if err != nil {
-		panic(err)
+	sendMessage := []string{
+		"ASCII",
+		"PROGRAMMING",
+		"PLUS",
 	}
 
-	request.Write(conn)
-	response, err := http.ReadResponse(
-		bufio.NewReader(conn), request,
-	)
-	if err != nil {
-		panic(err)
-	}
+	current := 0
+	var conn net.Conn = nil
+	for {
+		var err error
+		if conn == nil {
+			conn, err = net.Dial("tcp", "localhost:9999")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Access: %d\n", current)
+		}
 
-	dump, err := httputil.DumpResponse(response, true)
-	if err != nil {
-		panic(err)
+		request, err := http.NewRequest(
+			"POST",
+			"/",
+			strings.NewReader(sendMessage[current]),
+		)
+		if err != nil {
+			panic(err)
+		}
+		err = request.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+
+		response, err := http.ReadResponse(
+			bufio.NewReader(conn),
+			request,
+		)
+		if err != nil {
+			fmt.Println("Retry")
+			conn = nil
+			continue
+		}
+
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+		current++
+		if current == len(sendMessage) {
+			break
+		}
 	}
-	fmt.Println(string(dump))
+	conn.Close()
 }
