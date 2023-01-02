9a8de6ce18b907cda7f521483818200ac41d4ccc
Shunpoco <tkngsnsk313320@gmail.com>
2023-01-02 14:15:59 +0900

    HTTP client gzip

--- 6-tcp-http/client/main.go
+++ 6-tcp-http/client/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bufio"
+	"compress/gzip"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"os"
 	"strings"
 )
 
@@ -36,6 +39,7 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+		request.Header.Set("Accept-Encoding", "gzip")
 		err = request.Write(conn)
 		if err != nil {
 			panic(err)
@@ -56,6 +60,18 @@ func main() {
 			panic(err)
 		}
 		fmt.Println(string(dump))
+
+		defer response.Body.Close()
+		if response.Header.Get("Content-Encoding") == "gzip" {
+			reader, err := gzip.NewReader(response.Body)
+			if err != nil {
+				panic(err)
+			}
+			io.Copy(os.Stdout, reader)
+			reader.Close()
+		} else {
+			io.Copy(os.Stdout, response.Body)
+		}
 		current++
 		if current == len(sendMessage) {
 			break
